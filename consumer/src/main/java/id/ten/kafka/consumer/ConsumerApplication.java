0e5389138a8df4a77a8a485c594d95e42d35050c
teten <tennugraha777@gmail.com>
2020-12-20 17:53:25 +0700

    receive record

--- consumer/src/main/java/id/ten/kafka/consumer/ConsumerApplication.java
+++ consumer/src/main/java/id/ten/kafka/consumer/ConsumerApplication.java
@@ -1,13 +1,45 @@
 package id.ten.kafka.consumer;
 
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
 @SpringBootApplication
-public class ConsumerApplication {
+public class ConsumerApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumerApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Properties properties = new Properties();
+		properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
+		properties.put(ConsumerConfig.GROUP_ID_CONFIG, "test");
+		properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
+		properties.put("auto.commit.interval.ms", "1000");
+		properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+		properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+
+
+		KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+		consumer.subscribe(Arrays.asList("partition-topic"));
+
+		while(true) {
+			ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+			for(ConsumerRecord<String, String> record:records) {
+				System.out.println("REceive data "+record);
+			}
+		}
+
+	}
 }
