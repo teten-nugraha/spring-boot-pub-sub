d0b3ec8ed7080d4157f2aff6cc5f0438268eee41
teten <tennugraha777@gmail.com>
2020-12-20 17:43:09 +0700

    send record

--- producer/src/main/java/id/ten/kafka/producer/ProducerApplication.java
+++ producer/src/main/java/id/ten/kafka/producer/ProducerApplication.java
@@ -1,13 +1,36 @@
 package id.ten.kafka.producer;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Properties;
+
 @SpringBootApplication
-public class ProducerApplication {
+public class ProducerApplication  implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProducerApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Properties properties = new Properties();
+		properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
+		properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+		KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+		for(int i=0; i < 10; i++) {
+			ProducerRecord<String, String> record = new ProducerRecord<>("partition-topic","Data ke-"+i);
+			producer.send(record);
+		}
+
+		producer.close();
+	}
 }
